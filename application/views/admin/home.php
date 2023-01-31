ff2a095e7d00f941fedaa71518dfbae8a82c09c6
MohammadEndriono <mohammadendri@gmail.com>
2023-01-30 23:03:35 -0800

    upload sistem perpustakaan CI

---
+++ application/views/admin/home.php
@@ -0,0 +1,11 @@
+ <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-md-12">
+          <center>
+          <h2>WELCOME ADMIN</h2>
+          </center>
+        </div>
+      </div>
+    </section>
+<!-- /.content -->
---
+++ application/views/petugas/home.php
@@ -0,0 +1,11 @@
+ <!-- Main content -->
+ <section class="content">
+      <div class="row">
+        <div class="col-md-12">
+          <center>
+          <h2>WELCOME PETUGAS</h2>
+          </center>
+        </div>
+      </div>
+    </section>
+<!-- /.content -->
